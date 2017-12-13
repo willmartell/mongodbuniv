40aac020fe9cf05621b4581ef2866116bb1c4e9a
willmartell <willmartell@gmail.com>
2017-12-13 13:37:18 -0600

    Update import_to_mlab.sh

--- import_to_mlab.sh
+++ import_to_mlab.sh
@@ -1,1 +1,1 @@
-mongoimport -h ds161873.mlab.com:61873 -u willmartell -p turtleDove101! -d mongo_mlab posts__f4_m101j_m101js_m101p_52d94c86e2d423744501ce90.json
+mongoimport -h ds161873.mlab.com:61873 -u xxx -p xxx -d mongo_mlab posts__f4_m101j_m101js_m101p_52d94c86e2d423744501ce90.json
